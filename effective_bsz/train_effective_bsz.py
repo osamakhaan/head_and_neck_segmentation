c3d73b6335559145301d7c3fb908c5485f7f40e5
Muhammad Osama Khan <mok232@login-0-2.local>
2021-05-15 18:06:54 +0400

    refactored code

--- effective_bsz/train_effective_bsz.py
+++ effective_bsz/train_effective_bsz.py
@@ -1,26 +1,28 @@
+# Python imports
+import os, math, pdb, re, time
 from glob import glob
-import SimpleITK as sitk
-import os
-import numpy as np
 from tqdm import tqdm
 import argparse
+import numpy as np
+import logging
 
-import torch
-torch.backends.cudnn.enabled = True
-from torch.utils import data
-import math
+# ML imports
 from scipy.ndimage.interpolation import zoom
-import pdb
+from scipy.spatial.distance import dice
+import torch
 from torch import nn
 import torch.nn.functional as F
-from scipy.spatial.distance import dice
+torch.backends.cudnn.enabled = True
+from torch.utils import data
+from torch.utils.tensorboard import SummaryWriter
 
-from dataset_effective_bsz import DatasetStg1
+# Medical imaging specific imports
+import SimpleITK as sitk
 import monai
 from monai.utils import set_determinism
-from torch.utils.tensorboard import SummaryWriter
-import os, re, time
-import logging
+
+from dataset_effective_bsz import DatasetStg1
+from test_utils import test
 
 set_determinism(seed=0) # set deterministic training for reproducibility
 
@@ -118,38 +120,7 @@ def main():
         writer.add_scalar('Loss/train', trainloss, epoch)
         
         if epoch % 10 == 0:
-            test_dc = 0.0
-            test_per_class_dcs = torch.FloatTensor([0.0 for i in range(9)])
-            for idx, (x, y, _) in enumerate(testdataloader):
-                with torch.no_grad():
-                    x = torch.autograd.Variable(x.cuda())
-                    pred = model(x)
-                    pred = F.softmax(pred, dim=1)
-                    # convert pred to one hot
-                    max_idx = torch.argmax(pred.cpu(),dim=1,keepdim=True)
-                    one_hot_pred = torch.FloatTensor(pred.shape)
-                    one_hot_pred.zero_()
-                    one_hot_pred.scatter_(1,max_idx,1)
-
-                    tmp = monai.metrics.compute_meandice(one_hot_pred, y.cpu(), include_background=args.include_background)
-                    dc, per_class_dcs = tmp.mean(), tmp.mean(dim=0)
-                    test_dc += dc.item()
-                    test_per_class_dcs += per_class_dcs
-                    n_classes = 10 if args.include_background else 9
-                    for l in range(n_classes):
-                        if n_classes == 9:
-                            m = l+1
-                        else:
-                            m = l
-                        # gt
-                        monai.visualize.img2tensorboard.add_animated_gif(writer, f"Ex {idx}/Label {m}", y[:,m,:,:,:].permute(0,2,3,1).cpu(), max_out=x.shape[1], scale_factor=255, global_step=epoch)
-                        # pred
-                        monai.visualize.img2tensorboard.add_animated_gif(writer, f"Ex {idx}/Label {m}/DC", one_hot_pred[:,m,:,:,:].permute(0,2,3,1).cpu(), max_out=x.shape[1], scale_factor=255, global_step=epoch)
-                    # image
-                    monai.visualize.img2tensorboard.add_animated_gif(writer, f"Image {idx}/DC {round(dc.item(),3)}", x[0].permute(0,2,3,1).cpu(), max_out=x.shape[1], scale_factor=255, global_step=epoch)
-
-            test_dc = test_dc/len(testdataloader)
-            test_per_class_dcs /= len(testdataloader)
+            test_dc, test_per_class_dcs = test(testdataloader, args, epoch, writer, model)
             
             best_per_class_dcs = [max(l1,l2) for l1,l2 in zip(test_per_class_dcs, best_per_class_dcs)]
             if best_dc < test_dc:
